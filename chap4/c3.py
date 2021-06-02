2fe0d3f566234db9e7a0f67b56b9e0dd84f64d79
miyagi yoshiya <s21023@std.it.college.ac.jp>
2021-06-02 08:23:44 +0000

    challenge

---
+++ chap4/c3.py
@@ -0,0 +1,6 @@
+def f(a,b,c,d=4,e=5):
+    return a + b + c + d + e
+print(f(1,2,3))
+print(f(1,2,3)) 
+print(f(1,2,3)) 
+
---
+++ chap4/c4.py
@@ -0,0 +1,7 @@
+def f1(x):
+    return x / 2
+
+def f2(x):
+    return x * 4
+
+print(f2(f1(4)))
